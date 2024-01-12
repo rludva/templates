e520112105ad5d5c419c51a8eda9f69d34719481
Radomir Ludva <noreplay@radmi.cz>
2024-01-13 00:17:57 +0100

    Call the update_crc.yaml ansible playbook from the update-binaries.sh script..

--- chome/home/user1/.local/bin/update-binaries.sh
+++ chome/home/user1/.local/bin/update-binaries.sh
@@ -4,3 +4,4 @@ ANSIBLE_HOME="$HOME/.local/ansible"
 
 ansible-playbook $ANSIBLE_HOME/update_yt-dlp.yaml
 ansible-playbook $ANSIBLE_HOME/update_omc.yaml
+ansible-playbook $ANSIBLE_HOME/update_crc.yaml
--- chome/user1/.local/bin/update-binaries.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-ANSIBLE_HOME="$HOME/.local/ansible"
-
-ansible-playbook $ANSIBLE_HOME/update_yt-dlp.yaml
-ansible-playbook $ANSIBLE_HOME/update_omc.yaml
